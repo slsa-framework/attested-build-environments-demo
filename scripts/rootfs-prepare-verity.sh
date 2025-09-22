cea8ceecb29b237eb6f612449ca89f710137beae
Pavel Iakovenko <paveliak@github.com>
2025-09-21 20:32:08 -0400

    Do not mount temporary drive (will be used for overlay)

--- scripts/rootfs-prepare-verity.sh
+++ scripts/rootfs-prepare-verity.sh
@@ -6,6 +6,12 @@ echo Installing software necessary for verity measurement
 sudo apt update
 sudo apt-get install -y expect cryptsetup
 
+echo Patching up fstab
+# Use UEFI label for the EFI partition instead of UUID
+sed -i 's/UUID=[^\s]\+\(\s\+\/boot\/efi\)/LABEL=UEFI\1/' /etc/fstab
+# Remove the /mnt partition, it should already be used for overlay
+sed -i '/\/mnt/d' /etc/fstab
+
 echo "Fixing up GPT for the expanded disk"
 expect -c 'spawn sudo parted /dev/disk/azure/scsi1/lun0 print; expect "Warning: Not all of the space available*"; send "f\r"; expect eof'
 
